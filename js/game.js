ebb4dd3e2194a97d0ee0a12c0e0ba74833b667bf
NeoBLR <neoblr@bk,ru>
2022-01-07 15:03:26 +0200

    fix for mobile

--- js/game.js
+++ js/game.js
@@ -1,5 +1,5 @@
-let W = screen.width
-let H = screen.height
+let W = document.body.clientWidth
+let H = document.body.clientHeight
 let canvas = document.getElementById('canvas')
 let ctx = canvas.getContext('2d')
 
@@ -35,18 +35,18 @@ class Snakes {
     // teleport to inverse
 
     if (this.y / 32 <= -1) {
-      this.y = Math.floor(screen.height / 32 - 1) * 32
+      this.y = Math.floor(document.body.clientHeight / 32 - 1) * 32
     }
 
     if (this.x / 32 <= -1) {
-      this.x = Math.floor(screen.width / 32 - 1) * 32
+      this.x = Math.floor(document.body.clientWidth / 32 - 1) * 32
     }
 
-    if (this.y / 32 >= Math.floor(screen.height / 32 + 1)) {
+    if (this.y / 32 >= Math.floor(document.body.clientHeight / 32 + 1)) {
       this.y = 0
     }
 
-    if (this.x / 32 >= Math.floor(screen.width / 32)) {
+    if (this.x / 32 >= Math.floor(document.body.clientWidth / 32)) {
       this.x = 0
     }
     console.log()
@@ -126,12 +126,20 @@ let interface = (Score, Len) => {
 
   //
   ctx.textAlign = 'start'
-  ctx.fillText('maxX: ' + Math.floor(screen.width / 32), 0 + 10, 100 + 10)
+  ctx.fillText(
+    'maxX: ' + Math.floor(document.body.clientWidth / 32),
+    0 + 10,
+    100 + 10
+  )
 
   //
 
   ctx.textAlign = 'end'
-  ctx.fillText('maxY: ' + Math.floor(screen.height / 32), W - 10, 100 + 10)
+  ctx.fillText(
+    'maxY: ' + Math.floor(document.body.clientHeight / 32),
+    W - 10,
+    100 + 10
+  )
 }
 
 main()
@@ -144,8 +152,14 @@ let MainLoop = setInterval(() => {
 
 window.addEventListener('orientationchange', function (event) {
   let ori = window.orientation
-  W = ori == 90 || ori == -90 ? screen.height : screen.width
-  H = ori == 90 || ori == -90 ? screen.width : screen.height
+  W =
+    ori == 90 || ori == -90
+      ? document.body.clientHeight
+      : document.body.clientWidth
+  H =
+    ori == 90 || ori == -90
+      ? document.body.clientWidth
+      : document.body.clientHeight
 
   // Draw()
   DrawOnResize()
